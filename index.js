3259908b84123cc5277fddfc5aac23e52f7771f9
Tom MacWright <tom@macwright.org>
2014-04-16 17:14:10 -0400

    Use geojson-mapnikify, remove remaining url bits

--- index.js
+++ index.js
@@ -47,23 +47,10 @@ function Source(id, callback) {
         return callback('invalid geojson');
     }
 
-    var done = function(err) {
-        if (err) return callback(err);
-        return callback(null, this);
-    }.bind(this);
-
-    var generated = mapnikify(JSON.parse(data), retina);
-    this._xml = generated.xml;
-
-    if (generated.resources.length) {
-        var q = queue(10);
-        generated.resources.forEach(function(res) {
-            q.defer(loadMarker, res.replace('@2x', ''), retina);
-        });
-        q.awaitAll(done);
-    } else {
-        done();
-    }
+    var generated = mapnikify(JSON.parse(data), retina, function(err, xml) {
+        this._xml = xml;
+        callback(null, this);
+    }.bind(this));
 }
 
 /**
@@ -116,36 +103,3 @@ Source.prototype.getInfo = function(callback) {
 Source.registerProtocols = function(tilelive) {
     tilelive.protocols['overlaydata:'] = Source;
 };
-
-function loadMarker(id, retina, callback) {
-    var matchURL = /^(url)(?:-([^\(]+))()/;
-    var matchFile = /^(pin-s|pin-m|pin-l)(?:-([a-z0-9-]+))?(?:\+([0-9a-fA-F]{6}|[0-9a-fA-F]{3}))?$/;
-
-    var isurl = id.indexOf('url-') === 0;
-    var marker = id.match(isurl ? matchURL : matchFile);
-
-    if (!marker) {
-        return callback('Marker "' + marker + '" is invalid.');
-    }
-
-    var slug = id + (retina ? '@2x' : '') + '.png';
-
-    if (isurl) {
-        getUrlMarker({
-            label: marker[2]
-        }, function(err, data) {
-            if (err) return callback(err);
-            fs.writeFile(TMP + slug, data.image, callback);
-        });
-    } else {
-        getMarker({
-            name: marker[1],
-            label: marker[2],
-            tint: marker[3],
-            retina: retina
-        }, function(err, data) {
-            if (err) return callback(err);
-            fs.writeFile(TMP + slug, data.image, callback);
-        });
-    }
-}
