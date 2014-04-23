989a9613ad8a607f05cd0897a4aae4ef7faa7daf
Young Hahn <young@developmentseed.org>
2014-04-22 21:34:43 -0400

    Add complex geojson and use it.

--- benchmark/bench.js
+++ benchmark/bench.js
@@ -5,13 +5,13 @@ var Benchmark = require('benchmark'),
 
 var suite = new Benchmark.Suite();
 
-new Overlay('overlaydata://' + fs.readFileSync('./test/data/example.geojson', 'utf8'),
+new Overlay('overlaydata://' + fs.readFileSync('./test/data/complex.geojson', 'utf8'),
     function(err, source) {
         suite
         .add('#getTile', function(deferred) {
             var q = queue(4);
             for (var i = 0; i < 16; i++) q.defer(function(done) {
-                source.getTile(1, 0, 0, done);
+                source.getTile(11, 486, 779, done);
             });
             q.awaitAll(function() {
                 deferred.resolve();
