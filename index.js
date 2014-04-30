73334489a154c73d04097b178b975a34ed6c95c2
Dane Springmeyer <dane@mapbox.com>
2014-04-29 23:22:32 -0700

    block the main loop less  - allocating the mapnik.Image only when needed helps shave ms off benchmark

--- index.js
+++ index.js
@@ -62,14 +62,13 @@ function Source(id, callback) {
  */
 Source.prototype.getTile = function(z, x, y, callback) {
     var map = new mapnik.Map(256, 256);
-    var im = new mapnik.Image(256, 256);
 
     try {
         map.fromString(this._xml, {}, function(err) {
             if (err) return callback(err);
             map.bufferSize = 256;
             map.extent = sph.xyz_to_envelope(x, y, z);
-            map.render(im, {}, function(err, im) {
+            map.render(new mapnik.Image(256, 256), {}, function(err, im) {
                 if (err) return callback(err);
                 im.encode('png', function(err, res) {
                     callback(err, res);
