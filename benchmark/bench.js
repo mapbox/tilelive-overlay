1a2b61ce1c6bbe8bb3b697b367f4488ade7730b7
Dane Springmeyer <dane@mapbox.com>
2014-08-26 17:06:38 -0700

    sanity check image in bench

--- benchmark/bench.js
+++ benchmark/bench.js
@@ -14,6 +14,9 @@ new Overlay('overlaydata://' + fs.readFileSync('./test/data/complex.geojson', 'u
             for (var i = 0; i < 16; i++) q.defer(function(done) {
                 source.getTile(11, 486, 779, function(err,im) {
                     if (err) throw err;
+                    if (im.length != 3651) {
+                        throw new Error("image is not the length we expected: " + im.length);
+                    }
                     done();
                 });
             });
