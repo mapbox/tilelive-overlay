6f3849c31142123225dc258f7a530b3b31a80b30
Tom MacWright <tom@macwright.org>
2014-09-10 13:59:52 -0700

    Bump geojson-mapnikify to fix marker width, add fixtures

--- test/overlay.js
+++ test/overlay.js
@@ -1,5 +1,6 @@
 var test = require('tap').test,
     fs = require('fs'),
+    mapnik = require('mapnik'),
     Overlay = require('../');
 
 test('overlay', function(t) {
@@ -15,6 +16,26 @@ test('overlay', function(t) {
             t.equal(err, 'This source does not provide info', 'does not provide info');
         });
         t.end();
+    });
+});
+
+test('overlay - fixture', function(t) {
+    new Overlay('overlaydata://' + fs.readFileSync(__dirname + '/data/example.geojson', 'utf8'),
+        function(err, source) {
+        if (err) throw err;
+        t.notOk(err, 'no error returned');
+        t.ok(source, 'source created');
+        source.getTile(0, 0, 0, function(err, res) {
+            t.notOk(err, 'no error');
+            if (process.env.UPDATE) fs.writeFileSync(__dirname + '/data/fixture.png', res);
+            var expectImage = new mapnik.Image.open(__dirname + '/data/fixture.png');
+            var resultImage = new mapnik.Image.fromBytesSync(res);
+
+            // Allow < 2% of pixels to vary by > default comparison threshold of 16.
+            var pxThresh = resultImage.width() * resultImage.height() * 0.02;
+            var pxDiff = expectImage.compare(resultImage);
+            t.end();
+        });
     });
 });
 
